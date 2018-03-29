6c1f925032040d6f38a44981369e82ee4cd5ca5f
Cristinel <cristinel@projects-abroad.net>
2018-03-29 17:19:52 +0300

    Babel with Webpack

--- indecision-app/src/playground/counter-example.js
+++ indecision-app/src/playground/counter-example.js
@@ -5,29 +5,30 @@ class Counter extends React.Component {
         this.handleMinusOne = this.handleMinusOne.bind(this);
         this.handleReset = this.handleReset.bind(this);
         this.state = {
-            count: props.count
+            count: 0
         };
     }
+    componentDidMount() {
+        const stringCount = localStorage.getItem('count');
+        const count = parseInt(stringCount, 10);
+
+        if (!isNaN(count)) {
+            this.setState(() => ({ count }));
+        }
+    }
+    componentDidUpdate(prevProps, prevState) {
+        if (prevState.count !== this.state.count) {
+            localStorage.setItem('count', this.state.count);
+        }
+    }
     handleAddOne() {
-        this.setState((prevState) => {
-            return {
-                count: prevState.count + 1
-            }
-        });
+        this.setState((prevState) => ({ count: prevState.count + 1 }));
     }
     handleMinusOne() {
-        this.setState((prevState) => {
-            return {
-                count: prevState.count - 1
-            }
-        });
+        this.setState((prevState) => ({ count: prevState.count - 1 }));
     }
     handleReset() {
-        this.setState(() => {
-            return {
-                count: 0
-            }
-        });
+        this.setState(() => ({ count: 0 }));
     }
     render() {
         return (
@@ -39,10 +40,6 @@ class Counter extends React.Component {
             </div>
         )
     }
-}
-
-Counter.defaultProps = {
-    count: 0
 }
 
 ReactDOM.render(<Counter />, document.getElementById('app'));
