554dc4ca7135a9ab190c5046336830f69e99a7be
Cristinel <cristinel@projects-abroad.net>
2018-03-30 16:34:33 +0300

    End of Part 1

--- indecision-app/src/components/IndecisionApp.js
+++ indecision-app/src/components/IndecisionApp.js
@@ -48,7 +48,7 @@ export default class IndecisionApp extends React.Component {
     };
 
     render() {
-        const subtitle = 'Put your live in the hands of a computer';
+        const subtitle = 'Put your life in the hands of a computer';
         return (
             <div>
                 <Header subtitle={subtitle} />
